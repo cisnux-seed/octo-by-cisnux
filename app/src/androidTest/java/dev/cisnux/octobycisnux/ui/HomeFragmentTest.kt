66454d9f4cc99895910e6e2aae49d5ad9a5649e6
Fajra Risqulla <fajrarisqulla@gmail.com>
2023-03-24 11:12:18 +0700

    add androidTesting and unitTesting

---
+++ app/src/androidTest/java/dev/cisnux/octobycisnux/ui/HomeFragmentTest.kt
@@ -0,0 +1,17 @@
+package dev.cisnux.octobycisnux.ui
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class HomeFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
---
+++ app/src/main/java/dev/cisnux/octobycisnux/ui/SplashActivity.kt
@@ -0,0 +1,41 @@
+package dev.cisnux.octobycisnux
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import dev.cisnux.octobycisnux.databinding.ActivitySplashBinding
+import dev.cisnux.octobycisnux.ui.MainActivity
+import dev.cisnux.octobycisnux.viewmodels.SettingsViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val settingsViewModel: SettingsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        settingsViewModel.userThemePreferences.observe(this, ::setAppTheme)
+        lifecycleScope.launch {
+            delay(1500L)
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun setAppTheme(isDarkModeActive: Boolean) = AppCompatDelegate.setDefaultNightMode(
+        if (isDarkModeActive) AppCompatDelegate.MODE_NIGHT_YES
+        else AppCompatDelegate.MODE_NIGHT_NO
+    )
+}
